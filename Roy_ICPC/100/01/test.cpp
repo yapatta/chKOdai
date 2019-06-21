67c11db62970fc5ff7d4b9c671c64b394c9e5047
門脇　遼太郎 <ryotaro@kadowakiryoutarounoMacBook-Air.local>
2019-06-22 00:06:24 +0900

    hello world

---
+++ Roy_ICPC/100/01/test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	while(cin >> n,n!=0)
+	{
+		vector<int> s(n);
+		for(int i=0;i<n;i++)
+			cin >> s[i];
+		sort(s.begin(),s.end());
+		long long ans=0;
+		for(int i=1;i<n-1;i++)
+			ans+=s[i];
+		ans/=(n-2);
+		cout << ans << endl;
+	}
+
+
+
+
+
+
+}
